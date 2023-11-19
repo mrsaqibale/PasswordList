a227f0a9a54c11474061aed01defc42305fa1d97
codewithprofessor <kalikali72547254@gmail.com>
2023-11-20 01:39:01 +0500

    Add pass String: }s|P@>=3wRMB!ADR3

--- tool.py
+++ tool.py
@@ -4,14 +4,15 @@ input_file = '../in.txt'
 output_file = 'passList.txt'
 
 def commitMe(line):
+    print(line)
     subprocess.run(['git', 'add', '.'])
-    subprocess.run(['git', 'commit', '-m', f'Automated commit: {line}'])
+    subprocess.run(['git', 'commit', '-m', f'Add pass String: {line}'])
 def commit_and_push(line):
     with open(output_file, 'a') as outfile:
         outfile.write(line)
         commitMe(line)
             
 with open(input_file, 'r') as infile:
-    for i in range(5166):
+    for i in range(3169):
         line = infile.readline()
         commit_and_push(line)
